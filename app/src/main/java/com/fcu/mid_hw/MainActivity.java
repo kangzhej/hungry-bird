bedebcf8710cd02965d656a3b904c32d831c5592
wolfheluo <gm900411@gmail.com>
2022-04-30 23:09:30 +0800

    init

--- app/src/main/java/com/fcu/mid_hw/MainActivity.java
+++ app/src/main/java/com/fcu/mid_hw/MainActivity.java
@@ -3,12 +3,37 @@ package com.fcu.mid_hw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn1, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.logged_out);
+
+        btn1 = (Button) findViewById(R.id.button);
+        btn2 = (Button) findViewById(R.id.button2);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                //what btn1 does
+                break;
+            case R.id.button2:
+                //what btn2 does
+                break;
+            default:
+                break;
+        }
     }
 }
