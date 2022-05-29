756a1ac2311edb06ac58e9343a6d1c3d46842e57
D0852558 <kangzhej@gmail.com>
2022-05-29 21:00:01 +0800

    0529

--- app/src/main/java/com/fcu/mid_hw/MainActivity.java
+++ app/src/main/java/com/fcu/mid_hw/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btn1, btn2;
+    Button btn3;    //testing
     private Object AdapterView;
 
 
@@ -21,8 +22,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn1 = (Button) findViewById(R.id.button);
         btn2 = (Button) findViewById(R.id.button2);
 
+        btn3 = (Button) findViewById(R.id.button3); //testing
+
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
+
+        btn3.setOnClickListener(this);  //testing
 
 
     }
@@ -43,6 +48,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 intent2.setClass(MainActivity.this, RegitserActivity.class);
                 startActivity(intent2);
                 break;
+
+            case R.id.button3:
+                Intent intent3 = new Intent();
+                intent3.setClass(MainActivity.this,Discover_page.class);
+                startActivity(intent3);
+                break;
+
             default:
                 break;
         }
